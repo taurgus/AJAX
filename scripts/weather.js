e0f28a336705231cf824046795c8908f8d96277e
taurgus <100350635+taurgus@users.noreply.github.com>
2023-04-20 09:22:58 +0300

    lisäys

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,8 +1,9 @@
 function getWeather(event) {
     event.preventDefault(); // Estää defaultin
     
-    const location = document.getElementById('location').value;
-    const xml = new XMLHttpRequest();
+    const api = "5c69cea1adcc7b793afeaf500214c0c5"; //API key
+    const location = document.getElementById('location').value; //Valitaan location indexistä
+    const xml = new XMLHttpRequest(); //Uusi xml haku
     xml.onreadystatechange = function() {
       if (xml.readyState === XMLHttpRequest.DONE) {
         if (xml.status === 200 && xml.readyState === 4) { //Jos haku onnistuu
@@ -12,12 +13,16 @@ function getWeather(event) {
             <h2>Sää paikassa ${location}</h2>
             <p>Lämpötila: ${weatherData.main.temp}°C</p>
             <p>Kosteus: ${weatherData.main.humidity}%</p>
+            <p>Tuulennopeus: ${weatherData.wind.speed}%</p>
             <p>Taivas: ${weatherData.weather[0].description}</p>
           `;
+        } else if (this.readyState == 4 && this.status == 404) {
+          alert("Ei löydy! " + this.readyState + "   " + this.status);
+          return;
         } 
       }
-    };
-    xml.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=5c69cea1adcc7b793afeaf500214c0c5&units=metric`); // Open Weatherin API avaimella toteutetaan haku
+    }; //Seuraavassa kohdassa oli kaikkein suurin ongelma saada toimimaan
+    xml.open('GET', `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=5c69cea1adcc7b793afeaf500214c0c5`); // Open Weatherin API avaimella toteutetaan haku
     xml.send();
   }
   
