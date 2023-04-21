3d3b7d829a266c4ac779c5007cec5e3b9158f92a
taurgus <100350635+taurgus@users.noreply.github.com>
2023-04-21 12:57:12 +0300

    lissää

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,16 +1,18 @@
 function getWeather(event) {
     event.preventDefault(); // Estää defaultin
 
-
+    //Muuttujat
     const api = "5c69cea1adcc7b793afeaf500214c0c5"; //API key
     const location = document.getElementById('location').value; //Valitaan location indexistä
     const xml = new XMLHttpRequest(); //Uusi xml haku
+
     xml.onreadystatechange = function() {
       if (xml.readyState === XMLHttpRequest.DONE) {
         if (xml.status === 200 && xml.readyState === 4) { //Jos haku onnistuu
           const weatherData = JSON.parse(xml.responseText); //
           const weatherInfo = document.getElementById('weather-info'); //Weather-infoon parsitaan JSON tiedot esille
-          weatherInfo.innerHTML = `
+          //Eri säätiedot
+          weatherInfo.innerHTML = ` 
             <h2>Sää paikassa ${location}</h2>
             <p>Maa: ${weatherData.sys.country}</p>
             <p>Lämpötila: ${weatherData.main.temp}°C</p>
@@ -19,7 +21,7 @@ function getWeather(event) {
             <p>Pilvisyys: ${weatherData.clouds.all}</p>
             <p>Taivas: ${weatherData.weather[0].description}</p>
           `;
-        } else if (this.readyState == 4 && this.status == 404) {
+        } else if (this.readyState == 4 && this.status == 404) { //Jos kaupunkia ei löydy niin ilmoitus
           alert("Ei löydy! " + this.readyState + "   " + this.status);
           return;
         } 
